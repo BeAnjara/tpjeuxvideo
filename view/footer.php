f1a3d8a3267cd6554b1ff04b614c0b9aaf546b26
BeAnjara <beanjarahoussen@gmail.com>
2019-12-23 11:37:21 +0300

    View folder which contains header.php and footer.php

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+</div>
+<script src="lib/js/jquery-3.3.1.min.js"></script>
+<script src="lib/js/popper.min.js"></script>
+<script src="lib/js/bootstrap.min.js"></script>
+<script src="try_ajax.js"></script>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jeu vidéo</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="lib/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
